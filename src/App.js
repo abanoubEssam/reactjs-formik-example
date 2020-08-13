b02fbb635a655ed5c3a387b92d5ac61639e2dcb8
Abanoub Essam <20130130@fa-hists.edu.eg>
2020-08-14 00:47:48 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Formik, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+
+  onSubmit = (values) => {
+    console.log(values)
+  }
+
+  form = (props) => {
+    return (
+      <form onSubmit={props.handleSubmit}>
+        <Field name="name" placeholder="name" />
+        <ErrorMessage name="name" />
+        <br />
+        <Field name="email" placeholder="email" type="email" />
+        <br />
+        <Field name="type" component="select">
+          <option value="1">One</option>
+          <option value="2" >Two</option>
+        </Field>
+        <br />
+        <br />
+        <label>Active</label>
+        <Field name="active" type="checkbox" />
+        <br />
+        <br />
+        <label>Category</label><br />
+        <label>one</label>
+        <Field name="category" type="radio" value="1" /><br />
+        <label>two</label>
+        <Field name="category" type="radio" value="2" /><br />
+        <br />
+
+        <Field name="social.facebook" placeholder="facebook" />
+        <ErrorMessage name="social.facebook" />
+        <br />
+
+        <Field name="social.twitter" placeholder="twitter" />
+        <ErrorMessage name="social.twitter" />
+        <br />
+
+        <button type="submit">click</button>
+      </form>
+    )
+  }
+
+  schema = () => {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      active: Yup.string().required(),
+      category: Yup.string().required(),
+      social: Yup.object().shape({
+        facebook: Yup.string().required("facebook field required"),
+        twitter: Yup.string().required(),
+      })
+    })
+    return schema
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            type: "",
+            active: false,
+            category: "",
+            social: {
+              facebook: "",
+              twitter: ""
+            }
+          }}
+          onSubmit={this.onSubmit}
+          validationSchema={this.schema()}
+          render={this.form}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
